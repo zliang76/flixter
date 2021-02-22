ff32275ea2297aefa2bde09b27a7e0fccbf17781
Zifeng Liang <zifeng.liang@vanderbilt.edu>
2021-02-22 00:32:17 -0600

    Initial Commit

---
+++ flixter/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCellTableViewCell.swift
+//  flixter
+//
+//  Created by Zifeng Liang on 2/21/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
